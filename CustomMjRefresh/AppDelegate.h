3ea9b2138d7dacb7fb6214075a459493dcf71fdb
BigBagFind <594228924@qq.com>
2015-11-13 11:29:19 +0800

    Initial Commit

---
+++ CustomMjRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomMjRefresh
+//
+//  Created by 吴玉铁 on 15/11/13.
+//  Copyright © 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomMjRefresh/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomMjRefresh
+//
+//  Created by 吴玉铁 on 15/11/13.
+//  Copyright © 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
